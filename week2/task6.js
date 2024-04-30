d3dea077b585aaac43ba4bace0680f7646862966
yaow2114 <122402926+yaow2114@users.noreply.github.com>
2024-04-30 16:02:48 -0400

    complete week2

--- week2/task6.js
+++ week2/task6.js
@@ -1,3 +1,12 @@
 'use strict';
 
 var fs = require('fs');
+var readStream = fs.createReadStream('music.mp3');
+/* note:
+The line var writeStream = fs.createWriteStream('New_music.mp3'); 
+by itself does not create an actual file. 
+It only creates a writable stream object that is associated with the file path 'New_music.mp3'.
+*/
+var writeStream = fs.createWriteStream('New_music.mp3');
+
+readStream.pipe(writeStream);
